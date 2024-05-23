ffaaa2dea6fabe7084664401bcfbd20d6b5ca2b9
Victoria <victoria.unizhona@techmagic.co>
2024-05-23 22:34:48 +0300

    added http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FooterComponent} from "./shared/footer/footer.component";
 import {SharedModule} from "./shared/shared.module";
+import {TodoListComponent} from "./todo-list/todo-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule, FooterComponent],
+  imports: [RouterOutlet, SharedModule, FooterComponent, TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
